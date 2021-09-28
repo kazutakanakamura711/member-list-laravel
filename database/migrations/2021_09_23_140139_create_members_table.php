6c9111467cb7bd681ba2340079ef29e46cc872c1
kazutakanakamura711 <a711st777@gmail.com>
2021-09-28 20:25:44 +0900

    migration変更

--- database/migrations/2021_09_23_140139_create_members_table.php
+++ database/migrations/2021_09_23_140139_create_members_table.php
@@ -16,7 +16,7 @@ class CreateMembersTable extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('email');
             $table->timestamps();
             $table->integer('is_delete')->default(0);
